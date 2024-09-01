649d0fd06cd7ba75c7ea7263a2e856fe4dad7c7a
Kemal Kazanc <Keemalkazanc@gmail.com>
2024-09-02 01:19:00 +0200

    Initial Commit

---
+++ Projet_NomadApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Projet_NomadApp
+//
+//  Created by Kemal Kazanc on 02/09/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Projet_NomadApp/Projet_NomadAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Projet_NomadAppApp.swift
+//  Projet_NomadApp
+//
+//  Created by Kemal Kazanc on 02/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct Projet_NomadAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
